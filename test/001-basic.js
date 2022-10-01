d150cfb914b1b3043b5f839ac36bf44adc99ec74
Konstantin S. Uvarin <khedin@gmail.com>
2022-10-01 19:01:44 +0200

    [bug] add width, height getters; fix max/min bug (was defaulting to 0)

--- test/001-basic.js
+++ test/001-basic.js
@@ -1,16 +1,21 @@
 'use strict';
 
 const { expect } = require( 'chai' );
+const   refute   = require( 'refutable' );
 const { Ladder } = require( '../lib/ladder.js' );
 
 describe( 'Ladder', () => {
-    const kantor2 = [[1,0], [1,1], [1,1], [3,1], [1,1], [1,1], [1,1]];
+    const kantor2 = [[1,1], [1,1], [1,1], [3,1], [1,1], [1,1], [1,1]];
 
     it( 'can read & write', done => {
         const lad = new Ladder;
         lad.add(kantor2);
         console.log( lad );
         expect( lad.fetch() ).to.deep.equal( kantor2 );
+
+        expect( lad.width() ).to.equal(9);
+        expect( lad.height() ).to.equal(6);
+
         done();
     });
 
@@ -22,6 +27,12 @@ describe( 'Ladder', () => {
         console.log(graph);
 
         expect( graph.length ).to.equal(100);
+
+        for (let i of Array(3)) {
+            lad.add([[lad.width(), 1], ...lad.fetch()]);
+            console.log(lad.width())
+        };
+        console.log(lad.graph(100,100));
 
         done();
     });

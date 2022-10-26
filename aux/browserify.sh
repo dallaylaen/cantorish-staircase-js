e5c6d124ffe940c4b343239bbca77022072a5aaf
Konstantin S. Uvarin <khedin@gmail.com>
2022-10-26 17:34:05 +0200

    [repo] Add browserify

---
+++ aux/browserify.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -eux
+
+SRC=./lib/web-index.js
+DST=docs/js
+NAME=ladder
+
+mkdir -p "$SRC"
+browserify "$SRC" -d -o "$DST"/"$NAME".js
+browserify "$SRC" -p tinyify -o "$DST"/"$NAME".min.js
+

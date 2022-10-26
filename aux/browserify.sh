c85a8f082b089d67f357f50ad2a4482e248aedef
Konstantin S. Uvarin <khedin@gmail.com>
2022-10-27 01:32:06 +0200

    [repo] Fix bug in my browserify script

--- aux/browserify.sh
+++ aux/browserify.sh
@@ -6,7 +6,7 @@ SRC=./lib/web-index.js
 DST=docs/js
 NAME=ladder
 
-mkdir -p "$SRC"
+mkdir -p "$DST"
 browserify "$SRC" -d -o "$DST"/"$NAME".js
 browserify "$SRC" -p tinyify -o "$DST"/"$NAME".min.js
 
